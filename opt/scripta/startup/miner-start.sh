3adf26359604ee6d3f5b5a2f42c4a565efa72c18
mox235 <khudson235@gmail.com>
2014-03-23 12:16:25 -0500

    always use cgminer-gc3355 for now

--- opt/scripta/startup/miner-start.sh
+++ opt/scripta/startup/miner-start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 sudo /usr/sbin/ntpdate -u pool.ntp.org
-sudo /usr/bin/screen -dmS cgminer /opt/scripta/bin/cgminer -c /opt/scripta/etc/miner.conf
-
-#sudo /opt/scripta/bin/cgminer -c /opt/scripta/etc/miner.conf >> /dev/null &
-#echo $! > /opt/scripta/var/cgminer.lock
-
-
+sudo /usr/bin/screen -dmS cgminer /opt/scripta/bin/cgminer-gc3355 -c /opt/scripta/etc/miner.conf
