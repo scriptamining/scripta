5db42d1c1d28fc9ad576d03a9227b5cdfec2727d
richmondimage <richmondimage@me.com>
2014-04-17 14:23:01 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 /usr/bin/screen -S miner -X quit
 cd
 git pull
-sudo chown -R www-data /opt/scripta
-sudo chown -R www-data /var/www
+sudo chown -R www-data:www-data /opt/scripta
+sudo chown -R www-data:www-data /var/www
 sudo chmod -R 775 /opt/scripta/startup/*.sh
 sudo chmod -R 775 /etc/rc.local
 sleep 5
