4db35c97fff0c13beafc52233406c0999bdbdcd8
richmondimage <richmondimage@me.com>
2014-04-22 01:36:12 -0400

    Update miner-start.sh
    Watchdog updates

--- opt/scripta/startup/miner-start.sh
+++ opt/scripta/startup/miner-start.sh
@@ -1,8 +1,7 @@
 \#!/bin/bash
 sudo /usr/sbin/ntpdate -u pool.ntp.org
 sudo /usr/bin/screen -dmS bfgminer /opt/scripta/bin/bfgminer -c /opt/scripta/etc/miner.conf
-
-#sudo /opt/scripta/bin/cgminer -c /opt/scripta/etc/miner.conf >> /dev/null &
-#echo $! > /opt/scripta/var/cgminer.lock
+sleep 1
+echo `pidof bfgminer` > /opt/scripta/var/bfgminer.pid
 
 
