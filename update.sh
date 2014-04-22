9c2e556cc9188d1fad7ea3230cec9376ff1210a6
richmondimage <richmondimage@me.com>
2014-04-22 06:43:51 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 /usr/bin/screen -S miner -X quit
 cd
 git pull
+sudo cp -Ru /scripta/etc/rc.local /etc/rc.local
+sudo cp -Ru /scripta/opt/scripta/ /opt/
+sudo cp -Ru /scripta/var/ /
+sudo cp -Ru /scripta/update.sh /update.sh
 sudo chown -R www-data:www-data /opt/scripta
 sudo chown -R www-data:www-data /var/www
 sudo chmod -R 775 /opt/scripta/startup/*.sh
